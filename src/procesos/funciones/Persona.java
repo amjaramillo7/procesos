e7b879b566547d2545e1b518629f3eb7aba29557
AMJR <AMJR@172.17.54.219>
2014-06-06 09:02:26 -0500

    versionCorreccion4

--- src/procesos/funciones/Persona.java
+++ src/procesos/funciones/Persona.java
@@ -1,17 +1,21 @@
 package procesos.funciones;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 /**
  * @author amjaramillo7
  */
 
 public class Persona {
+    private static int BufferedReader;
     /*
     * Creación e incialización de las variables
     */
@@ -23,6 +27,7 @@ public class Persona {
         String Ciudad;
         String Sexo;
         String Direccion;
+       
         
         System.out.print("Nombre: ");
         Nombre_Completo = sc.nextLine();  //leer un String
@@ -42,18 +47,20 @@ public class Persona {
         System.out.print("Dirección: ");
         Direccion = sc.nextLine();  //leer un String
         
-        String datos = "Hola " + Nombre_Completo + " "+"este es el registro de tus datos ingresados:"+ "\r\n"+
+        String datos = "\r\n"+"\r\n"+"Hola " + Nombre_Completo + " "+"este es el registro de tus datos ingresados:"+ "\r\n"+
                        "Nombres Completos: "+Nombre_Completo+"\r\n"+
                        "Apellidos: "+Apellidos+"\r\n"+
                        "Cedula de ciudadania: "+Cedula+"\r\n"+
                        "Ciudad de Origen: "+Ciudad+"\r\n"+
-                       "Sexo: "+Sexo+"\r\n"+Direccion+"\r\n";
-        writeFile(datos);
+                       "Sexo: "+Sexo+"\r\n"+Direccion+"\r\n"+"\r\n"+"\r\n"+"\r\n"+"\r\n";
+        escribir(datos);
+        leer();
+        
         
     }
     public static String Path = "D:\\DATOS\\Desktop\\DatosPersona.txt";
     
-    public static void writeFile(String valor) throws IOException {
+    public static void escribir(String valor) throws IOException {
         File file = new File(Path); // Instanciamos la Clase File
         if (!file.exists()) { //Solo si el archivo no existe se lo crea, caso contrario se presenta una alerta]
             file.createNewFile();
@@ -62,7 +69,29 @@ public class Persona {
         buffer.write(valor);
         buffer.close();
     }
-   
+    public static void leer(){
+         BufferedReader br = null;
+ 		try {
+ 			String Line;
+ 			br = new BufferedReader(new FileReader(Path));
+ 			while ((Line = br.readLine()) != null) { //recorremos y presentamos el contenido del archivo de texto
+                             System.out.println(Line); // imprimimos cada linea del archivo
+ 			}
+  
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		} finally {
+ 			try {
+ 				if (br != null)br.close();
+ 			} catch (IOException ex) {
+ 				ex.printStackTrace();
+ 			}
+ 		}
+     
+ 
+ }
+
+    
     
 
 }
